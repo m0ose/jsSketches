e6982077d85ca21b13f2ba0286a68c1dac47c9dc
Cody <d00d3rs0n@gmail.com>
2021-10-19 14:57:11 -0600

    cleanup

---
+++ seekerPitch/linspace.js
@@ -0,0 +1,30 @@
+
+/**
+ * Return evenly spaced numbers over a specified interval
+ * 
+ * @param {number} start 
+ * @param {number} stop 
+ * @param {int} number 
+ * @param {boolean} includeStop 
+ * @param {Float64Array} DataType 
+ * @returns {Float64Array}
+ */
+export function linspace(
+  start,
+  stop,
+  number,
+  includeStop = true,
+  DataType = Float64Array
+) {
+  const result = new DataType(number + 1 * includeStop)
+  for (let x = 0; x < result.length; x++) {
+      result[x] = start + (stop - start) * (x / number)
+  }
+  return result
+}
+
+export function linspaceMiddle(start, stop, slotsCount) {
+  return linspace(start, stop, slotsCount, false).map(
+      (x) => x + (stop - start) / (2 * slotsCount)
+  )
+}
